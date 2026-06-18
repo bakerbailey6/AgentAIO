b7f44ef630e81fe6ed3175a7fd6135a9fb6f9191
bakerbailey6 <bakerbailey9@gmail.com>
2026-06-18 11:24:32 -0700

    feat: scaffold Tauri 2.0 + Next.js 15 project

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,7 @@
+// src-tauri/src/lib.rs
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
